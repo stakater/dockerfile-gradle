996533900e5ed2935ef84bedf26fd5887823bd32
hazim1093 <hazim_malik@hotmail.com>
2017-04-26 12:48:23 +0500

    Add resources for gradle docker image

---
+++ 3.5-alpine/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+_gradle_version=$1
+_gradle_tag=$2
+_release_build=false
+
+if [ -z "${_gradle_version}" ]; then
+	source GRADLE_VERSION
+	_gradle_version=$GRADLE_VERSION
+	_gradle_tag=$GRADLE_VERSION
+	_release_build=true
+fi
+
+echo "GRADLE_VERSION: ${_gradle_version}"
+echo "DOCKER TAG: ${_gradle_tag}"
+echo "RELEASE BUILD: ${_release_build}"
+
+docker build --build-arg GRADLE_VERSION=${_gradle_version} --tag "stakater/gradle-alpine:${_gradle_tag}"  --no-cache=true .
+
+if [ $_release_build == true ]; then
+	docker tag "stakater/gradle-alpine:${_gradle_tag}" "stakater/gradle-alpine:latest"
+fi
